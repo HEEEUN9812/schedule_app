5f6c628b6f3c962d5669f6fd59d7c33128a71ca7
HEEEUN9812 <heeeun9812@gmail.com>
2024-01-23 12:46:11 +0900

    오류 코드만 메세지는 고민중...

--- src/main/java/com/heeeun/my_schedule_appspring/service/ScheduleService.java
+++ src/main/java/com/heeeun/my_schedule_appspring/service/ScheduleService.java
@@ -4,8 +4,10 @@ import com.heeeun.my_schedule_appspring.dto.ScheduleRequestDto;
 import com.heeeun.my_schedule_appspring.dto.ScheduleResponseDto;
 import com.heeeun.my_schedule_appspring.entity.Schedule;
 import com.heeeun.my_schedule_appspring.repository.ScheduleRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -57,13 +59,13 @@ public class ScheduleService {
 
     public Schedule findSchedule(Long id){
         return scheduleRepository.findById(id).orElseThrow(()->
-                new IllegalArgumentException("선택한 일정은 존재하지 않습니다."));
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "선택한 일정이 존재하지 않습니다."));
     }
 
     public void confirmPassword (ScheduleRequestDto requestDto, Schedule schedule){
         if(schedule.getPassword().equals(requestDto.getPassword())){
         }else {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"비밀번호가 일치하지 않습니다.");
         }
     }
 }
